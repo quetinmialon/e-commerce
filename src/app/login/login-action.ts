14458dbe1540e6857ef0113fa51407ad9a12f79e
Maxime Le Duc <maximeld19@gmail.com>
2024-03-19 16:54:25 +0100

    Move use server

--- src/app/login/login-action.ts
+++ src/app/login/login-action.ts
@@ -1,9 +1,7 @@
-
-
+'use server';
 import {loginUseCase} from "@/index";
 
 export const loginAction = async (formData: FormData) => {
-    'use server';
     const email = formData.get('email') as string;
     const password = formData.get('password') as string;
     await loginUseCase(email, password)
